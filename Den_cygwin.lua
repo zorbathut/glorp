c234de268a1d3bbb10aa7782efbad0b5c53cc49d
Ben Wilhelm <zorba-git-osx@pavlovian.net>
2009-12-26 22:19:34 -0800

    so, osx installer?

--- Den_cygwin.lua
+++ Den_cygwin.lua
@@ -34,62 +34,24 @@ do
   end
   table.insert(data, ursa.rule{"build/deploy/licenses.txt", "glorp/resources/licenses.txt", ursa.util.system_template{"cp $SOURCE $TARGET"}})
 
-  -- generic copy-a-lot framework
-  local function copy_a_lot(token, destprefix, sourceprefix)
-    ursa.token.rule{token .. "_copy", "#" .. token .. "_files", function ()
-        local data_items = {}
-        for k in ursa.token{token .. "_files"}:gmatch("[^%s]+") do
-          local ext = k:match("^.*%.([^%.]+)$")
-          
-          local dst = "build/deploy/" .. destprefix .. k
-          local src = sourceprefix .. k
-          
-          if ext == "png" then
-            table.insert(data_items, ursa.rule{dst, src, ursa.util.system_template{"pngcrush -brute -rem alla -cc $SOURCE $TARGET"}})
-          elseif ext == "wav" then
-            dst = dst:gsub("%.wav", ".ogg")
-            table.insert(data_items, ursa.rule{dst, src, ursa.util.system_template{"oggenc --downmix -q 5 -o $TARGET $SOURCE || oggenc -q 6 -o $TARGET $SOURCE"}})
-          else
-            table.insert(data_items, ursa.rule{dst, src, ursa.util.system_template{"cp $SOURCE $TARGET"}})
-          end
-        end
-        return data_items
-      end, always_rebuild = true}
-    return ursa.util.token_deferred{token .. "_copy"}
-  end
+  -- second we generate our actual data copies
+  ursa.token.rule{"built_data", "#datafiles", function ()
+    local items = {}
+    for _, v in pairs(ursa.token{"datafiles"}) do
+      table.insert(items, ursa.rule{("build/deploy/%s"):format(v.dst), v.src, ursa.util.system_template{v.cli}})
+    end
+    return items
+  end, always_build = true}
 
-  -- replicate the data structure in
-  ursa.token.rule{"data_files", nil, "cd data && find -type f | sed s*\\\\./**", always_rebuild = true}
-  table.insert(data, copy_a_lot("data", "data/", "data/"))
-
-  ursa.token.rule{"stock_files", nil, "cd glorp/resources && ls mandible_games.png", always_rebuild = true}
-  table.insert(data, copy_a_lot("stock", "data/", "glorp/resources/"))
-
-  -- JIT lua files
-  ursa.token.rule{"jit_files", nil, "cd glorp/resources/jit && ls"}
-  table.insert(data, copy_a_lot("jit", "data/jit/", "glorp/resources/jit/"))
-
-  -- Lua files in Glorp
-  ursa.token.rule{"luaglorp_files", nil, "cd glorp && ls *.lua | grep -v Den"}
-  table.insert(data, copy_a_lot("luaglorp", "data/", "glorp/"))
-
-  -- Lua files and font files in our core
-  ursa.token.rule{"core_files", nil, "ls *.lua *.ttf"}
-  table.insert(data, copy_a_lot("core", "", ""))
-
-    
-  ursa.token.rule{"data", data, ""}
-
-
-  ursa.token.rule{"installers", {data, "#version"}, function ()
+  ursa.token.rule{"installers", {data, ursa.util.token_deferred{"built_data"}, "#version"}, function ()
     local v = ursa.token{"version"}
     
     local exesuffix = ("%s-%s.exe"):format(params.name, v)
     local exedest = "build/" .. exesuffix
     print("ED:", exedest)
     ursa.rule{"build/installer.nsi", {data, "glorp/installer.nsi.template"}, function(dst, src)
-      local files = ursa.util.system{"cd build/deploy && find -type f | sed s*\\\\./**"}
-      local dir = ursa.util.system{"cd build/deploy && find -type d | sed s*\\\\./**"}
+      local files = ursa.util.system{"cd build/deploy && find . -type f | sed s*\\\\./**"}
+      local dir = ursa.util.system{"cd build/deploy && find . -type d | sed s*\\\\./**"}
       
       local inp = io.open("glorp/installer.nsi.template", "rb")
       local otp = io.open("build/installer.nsi", "w")
@@ -139,6 +101,6 @@ do
   end, always_rebuild = true}
 end
 
-rv.installers = ursa.util.token_deferred{"installers"}
+rv.installers = function () return ursa.util.token_deferred{"installers"} end
 
 return rv
--- Den_osx.lua
+++ Den_osx.lua
@@ -6,8 +6,10 @@ rv.cxx_flags = "-arch i386 -Fglorp/Glop/build/Glop -DMACOSX -I/opt/local/include
 rv.ld_flags = "-arch i386 -Lglorp/Glop/Glop/OSX/lib -Fglorp/Glop/build/Glop -framework OpenGL -framework Carbon -framework AGL -framework ApplicationServices -framework IOKit -framework Glop -ljpeg6b -lfreetype235"
 rv.extension = ".prog"  -- have to use something or it'll conflict
 
+local runnable_deps
+
 rv.create_runnable = function(dat)
-  local basepath = "build/" .. params.name .. ".app"
+  local basepath = "build/" .. params.longname .. ".app"
   
   local runnable = {}
   
@@ -15,7 +17,7 @@ rv.create_runnable = function(dat)
   local current_glop_iteration = 0
   
   -- copy our main executable
-  table.insert(runnable, ursa.rule{basepath .. "/Contents/MacOS/" .. params.name, dat.mainprog, ursa.util.system_template{"cp $SOURCE $TARGET"}})
+  table.insert(runnable, ursa.rule{basepath .. "/Contents/MacOS/" .. params.longname, dat.mainprog, ursa.util.system_template{"cp $SOURCE $TARGET"}})
   
   local function tweak_glop(cli)
     current_glop_iteration = current_glop_iteration + 1
@@ -33,7 +35,72 @@ rv.create_runnable = function(dat)
   -- copy the main glop library
   table.insert(runnable, ursa.rule{basepath .. "/Contents/Frameworks/Glop.framework/Glop", current_glop, ursa.util.system_template{"cp $SOURCE $TARGET"}})
   
-  return {deps = runnable, cli = basepath .."/Contents/MacOS/" .. params.name}
+  runnable_deps = runnable
+  
+  return {deps = runnable, cli = basepath .."/Contents/MacOS/" .. params.longname}
+end
+
+-- installers
+function rv.installers()
+  assert(runnable_deps)
+  local app_prefix = ("build/deploy/%s.app/"):format(params.longname)
+  
+  local binaries = {}
+  
+  -- first we mirror our run structure over, plus stripping (oh baby oh baby)
+  -- hackery: pulling out filenames from literals
+  -- this will be removed once we have a better system for it
+  for k, v in pairs(runnable_deps) do
+    local sufix = v[1]:match(("@build/%s.app/(.*)"):format(params.longname))
+    assert(sufix)
+    
+    table.insert(binaries, ursa.rule{app_prefix .. sufix, v[1], ursa.util.system_template{"strip -S -x -o $TARGET $SOURCE"}})
+  end
+  
+  -- second we generate our actual data copies
+  ursa.token.rule{"built_data", "#datafiles", function ()
+    local items = {}
+    for _, v in pairs(ursa.token{"datafiles"}) do
+      table.insert(items, ursa.rule{app_prefix .. "Contents/Resources/" .. v.dst, v.src, ursa.util.system_template{v.cli}})
+    end
+    return items
+  end, always_build = true}
+  
+  local icon = ursa.rule{app_prefix .. "Contents/Resources/mandible.icns", "glorp/resources/mandicon.png", ursa.util.system_template{("makeicns -in $SOURCE -out $TARGET")}}
+  
+  local infoplist = ursa.rule{app_prefix .. "Contents/Info.plist", "#version", function ()
+    print("Writing info.plist")
+    local pl = io.open(app_prefix .. "Contents/Info.plist", "w")
+    
+    pl:write([[
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>CFBundleDevelopmentRegion</key>
+  <string>English</string>
+  <key>CFBundleExecutable</key>
+  <string>]] .. params.longname .. [[</string>
+  <key>CFBundleIconFile</key>
+  <string>mandible.icns</string>
+  <key>CFBundleIdentifier</key>
+  <string>com.mandible-games.]] .. params.name .. [[</string>
+  <key>CFBundleInfoDictionaryVersion</key>
+  <string>6.0</string>
+  <key>CFBundleName</key>
+  <string>]] .. params.longname .. [[</string>
+  <key>CFBundlePackageType</key>
+  <string>APPL</string>
+  <key>CFBundleVersion</key>
+  <string>]] .. ursa.token{"version"} .. [[</string>
+</dict>
+</plist>]])
+    
+    pl:close()
+    
+  end}
+  
+  return ursa.rule{("build/%s.dmg"):format(ursa.token{"outputprefix"}), {binaries, ursa.util.token_deferred{"built_data"}, infoplist, icon}, ursa.util.system_template{('hdiutil create -srcfolder "build/deploy/%s.app" $TARGET -ov'):format(params.longname)}}
 end
 
 return rv
