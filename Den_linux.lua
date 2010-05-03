63b6b3d8bbf9186ca11f25aea966a3b54807f889
Ben Wilhelm <zorba-glorp-ubuntu@pavlovian.net>
2010-05-03 09:25:48 -0700

    urrrgh

--- Den_linux.lua
+++ Den_linux.lua
@@ -33,80 +33,55 @@ end
 
 -- installers
 function rv.installers()
-  return {}
-  --[=[
-  assert(runnable_deps)
-  local app_prefix = ("build/osx/deploy/%s.app/"):format(params.longname)
+  -- first we have to build the entire path layout
+  local data = {}
   
-  local binaries = {}
-  
-  -- first we mirror our run structure over, plus stripping (oh baby oh baby)
-  for k, v in pairs(ursa.relative_from{runnable_deps}) do
-    local sufix = v:match(("build/osx/[-%s '.]*.app/(.*)"):format(params.longname))
-    assert(sufix)
-    
-    table.insert(binaries, ursa.rule{app_prefix .. sufix, v, ursa.util.system_template{"strip -S -x -o $TARGET $SOURCE"}})
-  end
-  
-  -- copy the reporter
-  table.insert(binaries, ursa.rule{app_prefix .. "Contents/Resources/reporter", params.builddir .. "reporter.prog", ursa.util.system_template{"strip -S -x -o $TARGET $SOURCE"}})
+  local datadir = params.builddir .. "deploy/" .. params.longname .. "/"
 
-  -- here's our bootstrapper for sane version errors
-  table.insert(binaries, ursa.rule{app_prefix .. "Contents/MacOS/" .. params.longname .. "-SystemVersionCheck", "glorp/resources/SystemVersionCheck", ursa.util.system_template{"cp $SOURCE $TARGET"}})
-  
+  -- DLLs and executables
+  local stripped = ursa.rule{params.builddir .. params.name .. ".prog.stripped", params.builddir .. params.name .. ".prog", ursa.util.system_template{"cp $SOURCE $TARGET && strip -s $TARGET"}}
+  table.insert(data, ursa.rule{datadir .. params.name, stripped, function ()
+    print("install_name_tool fakeout ugliness")
+    local ni = io.open(params.builddir .. params.name .. ".prog.stripped", "rb")
+    local dat = ni:read("*a")
+    ni:close()
+    
+    local orig = "glorp/Glop/release/linux/lib/libfmodex.so"
+    local new = "data/libfmodex.so"
+    while #new < #orig do
+      new = new .. "\0"
+    end
+    
+    local ndat = dat:gsub(orig, new)
+    
+    assert(#dat == #ndat)
+    
+    local out = io.open(datadir .. params.name, "wb")
+    out:write(ndat)
+    out:close()
+    
+    ursa.system{"chmod +x " .. datadir .. params.name}
+  end})
+  --table.insert(data, ursa.rule{datadir .. "data/reporter", params.builddir .. "reporter.prog", ursa.util.system_template{"cp $SOURCE $TARGET && strip -s $TARGET"}})
+  table.insert(data, ursa.rule{datadir .. "data/libfmodex.so", params.builddir .. "libfmodex.so", ursa.util.copy{}})
+  table.insert(data, ursa.rule{datadir .. "data/licenses.txt", "glorp/resources/licenses.txt", ursa.util.copy{}})
+
   -- second we generate our actual data copies
   ursa.token.rule{"built_data", "#datafiles", function ()
     local items = {}
     for _, v in pairs(ursa.token{"datafiles"}) do
-      table.insert(items, ursa.rule{app_prefix .. "Contents/Resources/" .. v.dst, v.src, ursa.util.system_template{v.cli}})
+      table.insert(items, ursa.rule{(datadir .. "%s"):format(v.dst), v.src, ursa.util.system_template{v.cli}})
     end
-    --assert(false)
     return items
   end, always_rebuild = true}
+  
+  cull_data(datadir, {data})
 
+  local installers = {}
   
-  local icon = ursa.rule{app_prefix .. "Contents/Resources/mandible.icns", "glorp/resources/mandicon.png", ursa.util.system_template{("glorp/resources/makeicns -in $SOURCE -out $TARGET")}}
+  table.insert(installers, ursa.rule{("build/%s-%s.tgz"):format(params.midname, ursa.token{"version"}), {data, ursa.util.token_deferred{"built_data"}, "#culled_data", "#version"}, ursa.util.system_template{("cd %sdeploy && tar -pczf ../../%s-%s.tgz \"%s\""):format(params.builddir, params.midname, ursa.token{"version"}, params.longname)}})
   
-  local infoplist = ursa.rule{app_prefix .. "Contents/Info.plist", "#version", function ()
-    print("Writing info.plist")
-    local pl = io.open(app_prefix .. "Contents/Info.plist", "w")
-    
-    pl:write([[
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-  <key>CFBundleDevelopmentRegion</key>
-  <string>English</string>
-  <key>CFBundleExecutable</key>
-  <string>]] .. params.longname .. [[-SystemVersionCheck</string>
-  <key>CFBundleIconFile</key>
-  <string>mandible.icns</string>
-  <key>CFBundleIdentifier</key>
-  <string>com.mandible-games.]] .. params.name .. [[</string>
-  <key>CFBundleInfoDictionaryVersion</key>
-  <string>6.0</string>
-  <key>CFBundleName</key>
-  <string>]] .. params.longname .. [[</string>
-  <key>CFBundlePackageType</key>
-  <string>APPL</string>
-  <key>CFBundleVersion</key>
-  <string>]] .. ursa.token{"version"} .. [[</string>
-  <key>LSEnvironment</key>
-  <dict>
-      <key>MinimumSystemVersion</key>
-      <string>10.6.0</string>
-  </dict>
-</dict>
-</plist>]])
-    
-    pl:close()
-    
-  end}
-  
-  cull_data(app_prefix, {binaries, icon, infoplist})
-  
-  return ursa.rule{("build/%s.dmg"):format(ursa.token{"outputprefix"}), {binaries, ursa.util.token_deferred{"built_data"}, "#culled_data", infoplist, icon}, ursa.util.system_template{('hdiutil create -srcfolder "build/osx/deploy/%s.app" $TARGET -ov'):format(params.longname)}}]=]
+  return installers
 end
 
 return rv
