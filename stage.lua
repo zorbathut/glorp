8f676b0cf6c8af8f79f91a4f13b241dbaa39c79d
Ben Wilhelm <zorba-git@pavlovian.net>
2010-06-25 16:37:18 -0700

    okay it sort of semi-runs

--- stage.lua
+++ stage.lua
@@ -229,6 +229,7 @@ function runuifile(file, ...)
   env.CreateFrame = function (type, parent) return CreateFrame(type, parent or env.UIParent) end
   env.GlorpController = Handle
   env.loadfile = function (...)
+    print("envlf")
     local dat, rv = loadfile(...)
     if dat then setfenv(dat, env) end
     return dat, rv
