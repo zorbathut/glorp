4b60d82535e6b998998578b6bced2b2756f98c64
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2013-06-30 05:22:52 -0700

    remove frame timings, replace with count in seconds

--- init_stdlib.lua
+++ init_stdlib.lua
@@ -1,18 +1,24 @@
 assert(loadfile("glorp/init_stdlib_coro.lua"))()
 
+local tps = 60
+
 local tick = External.Command.Event.Create(External, "System.Tick")
 local lasttick = External.Inspect.System.Time.Real()
 External.Event.System.Update.Begin:Attach(function ()
   local ticksdone = 0
-  while lasttick + 1/60 < External.Inspect.System.Time.Real() do
+  while lasttick + 1/tps < External.Inspect.System.Time.Real() do
     ticksdone = ticksdone + 1
-    lasttick = lasttick + 1/60
+    lasttick = lasttick + 1/tps
     tick()
     if ticksdone > 3 then
       -- ran out of frames, kill kill kill
       lasttick = External.Inspect.System.Time.Real()
     end
   end
+end)
+
+External.Command.Environment.Insert(External, "Utility.TicksFromSeconds", function (s)
+  return s * tps
 end)
 
 -- must be last
